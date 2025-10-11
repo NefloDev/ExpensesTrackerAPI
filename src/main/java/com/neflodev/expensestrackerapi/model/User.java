4acce95444622079b61e0ba4aacdf21abd99acc4
NefloDev <neflodeveloper@gmail.com>
2025-10-11 13:43:11 +0200

    build: create user entity and user repository

---
+++ src/main/java/com/neflodev/expensestrackerapi/model/User.java
@@ -0,0 +1,72 @@
+package com.neflodev.expensestrackerapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TBL_USER")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "USERNAME", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "CREATION_DATE")
+    private LocalDate creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "UPDATE_DATE")
+    private LocalDate updateDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
