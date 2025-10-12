2df6fba65b346eda69f6a019ce01041fe77e5677
NefloDev <neflodeveloper@gmail.com>
2025-10-12 22:17:08 +0200

    update: changed empty finding of user's accounts

--- src/main/java/com/neflodev/expensestrackerapi/service/AccountService.java
+++ src/main/java/com/neflodev/expensestrackerapi/service/AccountService.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 import java.util.Set;
@@ -38,7 +39,7 @@ public class AccountService {
 
         List<Account> userAccounts = repository.findByUser_Id(user.getId());
         if (userAccounts.isEmpty()) {
-            throw new NotFoundException("This user doesn't have any accounts.");
+            return new ArrayList<>();
         }
 
         return userAccounts.stream().map(mapper::accountToDto).toList();
