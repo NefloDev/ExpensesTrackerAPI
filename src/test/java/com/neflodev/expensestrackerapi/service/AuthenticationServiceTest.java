739c5af8f72efdf46216a5667678127684380504
NefloDev <neflodeveloper@gmail.com>
2025-10-12 19:47:52 +0200

    build: create test class for Authentication Service

---
+++ src/test/java/com/neflodev/expensestrackerapi/service/AuthenticationServiceTest.java
@@ -0,0 +1,49 @@
+package com.neflodev.expensestrackerapi.service;
+
+import com.neflodev.expensestrackerapi.dto.LoginUserDTO;
+import com.neflodev.expensestrackerapi.dto.RegisterUserDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AuthenticationServiceTest {
+
+    @Autowired
+    AuthenticationService service;
+
+    @Test
+    void testIsUserRegisteredTrue() {
+        RegisterUserDTO temp = new RegisterUserDTO("User", "password", "User", "User");
+
+        assertTrue(service.isUserRegistered(temp));
+    }
+
+    @Test
+    void testIsUserRegisteredFalse() {
+        RegisterUserDTO temp = new RegisterUserDTO("UserName2", "password2", "User2", "User2");
+
+        assertFalse(service.isUserRegistered(temp));
+    }
+
+    @Test
+    void signup() {
+        RegisterUserDTO temp = new RegisterUserDTO("UserName3", "password3", "User3", "User3");
+
+        assertNotNull(service.signup(temp));
+        assertTrue(service.isUserRegistered(temp));
+    }
+
+    @Test
+    void authenticate() {
+        LoginUserDTO temp = new LoginUserDTO("User", "password");
+
+        assertNotNull(service.authenticate(temp));
+    }
+}
