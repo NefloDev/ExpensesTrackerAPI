57388bfd073f146a81dab52d09789932c920684b
NefloDev <neflodeveloper@gmail.com>
2025-10-12 22:17:30 +0200

    build: create account service tests

---
+++ src/test/java/com/neflodev/expensestrackerapi/service/AccountServiceTest.java
@@ -0,0 +1,64 @@
+package com.neflodev.expensestrackerapi.service;
+
+import com.neflodev.expensestrackerapi.dto.AccountCreateParams;
+import com.neflodev.expensestrackerapi.dto.AccountDto;
+import com.neflodev.expensestrackerapi.exception.custom.NotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AccountServiceTest {
+
+    @Autowired
+    AccountService service;
+
+    @Test
+    void retrieveUserAccountsExisting() {
+        List<AccountDto> accounts = service.retrieveUserAccounts("User");
+
+        assertNotNull(accounts);
+        assertFalse(accounts.isEmpty());
+    }
+
+    @Test
+    void retrieveAccountsEmpty() {
+        List<AccountDto> accounts = service.retrieveUserAccounts("UserNoAccounts");
+
+        assertNotNull(accounts);
+        assertTrue(accounts.isEmpty());
+    }
+
+    @Test
+    void retrieveAvailableCurrencies() {
+        List<String> currencies = service.retrieveAvailableCurrencies();
+
+        assertNotNull(currencies);
+        assertFalse(currencies.isEmpty());
+    }
+
+    @Test
+    void createAccount() {
+        String username = "UserNoAccounts";
+        AccountCreateParams params = new AccountCreateParams("NewAccount", "EUR");
+
+        assertNotNull(service.createAccount(username, params));
+    }
+
+    @Test
+    void deleteAccount() {
+        assertDoesNotThrow(() -> service.deleteAccount(998L));
+    }
+
+    @Test
+    void deleteAccountNotFound() {
+        assertThrows(NotFoundException.class, () -> service.deleteAccount(2L));
+    }
+}
